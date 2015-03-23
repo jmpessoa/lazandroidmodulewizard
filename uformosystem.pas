b92c51bb679c594334ab957ef9b8cf881541ee4b
Anton <odisey1245@mail.ru>
2015-03-23 21:27:03 +0300

    * ide_tools: FormOSystem: fixed layout

--- uformosystem.pas
+++ uformosystem.pas
@@ -6,15 +6,14 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls, Buttons, inifiles, LazIDEIntf;
+  ExtCtrls, Buttons, ButtonPanel, inifiles, LazIDEIntf;
 
 type
 
   { TFormOSystem }
 
   TFormOSystem = class(TForm)
-    BitBtn1: TBitBtn;
-    Panel1: TPanel;
+    ButtonPanel1: TButtonPanel;
     RadioGroup1: TRadioGroup;
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
@@ -57,15 +56,32 @@ procedure TFormOSystem.FormCreate(Sender: TObject);
 var
   fileName: string;
 begin
+  { workaround for underline bug in GTK+ }
+  {$ifdef LCLGTK2}
+  RadioGroup1.Items[3] := StringReplace(RadioGroup1.Items[3], '_', '__', []);
+  {$endif}
   fileName:= AppendPathDelim(LazarusIDE.GetPrimaryConfigPath) + 'JNIAndroidProject.ini';
   if FileExistsUTF8(fileName) then
   begin
     with TIniFile.Create(fileName) do
     try
       PrebuildOSYS:= ReadString('NewProject','PrebuildOSYS', '');
-      if  PrebuildOSYS <> '' then
-         RadioGroup1.ItemIndex:= GetIndex(PrebuildOSYS)
-      else RadioGroup1.ItemIndex:= 0;
+      if PrebuildOSYS <> '' then
+        RadioGroup1.ItemIndex:= GetIndex(PrebuildOSYS)
+      else
+      {$ifdef windows}
+        RadioGroup1.ItemIndex:= 0;
+      {$else}
+        {$ifdef darvin}
+        RadioGroup1.ItemIndex:= 2;
+        {$else}
+          {$ifdef cpu64}
+        RadioGroup1.ItemIndex:= 3;
+          {$else}
+        RadioGroup1.ItemIndex:= 1;
+          {$endif}
+        {$endif}
+      {$endif}
     finally
       Free;
     end;
