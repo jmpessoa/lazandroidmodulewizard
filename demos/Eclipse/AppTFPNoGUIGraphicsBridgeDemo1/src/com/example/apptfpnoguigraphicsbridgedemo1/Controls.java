25ec4bd726e6471bf89aadd072d3025b4ca5af42
Jose Marques Pessoa <jmpessoa_hotmail.com>
2015-05-17 15:22:46 -0300

    Minor fix: AppTFPNoGUIGraphicsBridgeDemo1
    Minor fix: AppTFPNoGUIGraphicsBridgeDemo1 ".java"

--- demos/Eclipse/AppTFPNoGUIGraphicsBridgeDemo1/src/com/example/apptfpnoguigraphicsbridgedemo1/Controls.java
+++ demos/Eclipse/AppTFPNoGUIGraphicsBridgeDemo1/src/com/example/apptfpnoguigraphicsbridgedemo1/Controls.java
@@ -11935,138 +11935,6 @@ class jDigitalClock extends DigitalClock /*TextClock*/ { //please, fix what GUI 
 	  
 } //end class
 
-
-class jCustomViewLayout extends RelativeLayout {
-
-	   private long       pascalObj = 0;    // Pascal Object
-	   private Controls   controls  = null; // Control Class for events
-
-	   private Context context = null;
-	   private ViewGroup parent   = null;         // parent view
-	   private LayoutParams lparams;              // layout XYWH
-	   private int lparamsAnchorRule[] = new int[30];
-	   private int countAnchorRule = 0;
-	   private int lparamsParentRule[] = new int[30];
-	   private int countParentRule = 0;
-	   private int lparamH = 100;
-	   private int lparamW = 100;
-	   private int marginLeft = 0;
-	   private int marginTop = 0;
-	   private int marginRight = 0;
-	   private int marginBottom = 0;
-	   boolean mRemovedFromParent = false; //no parent!   
-	   
-	  //GUIDELINE: please, preferentially, init all yours params names with "_", ex: int _flag, String _hello ...
-	   public jCustomViewLayout(Controls _ctrls, long _Self) { //Add more others news "_xxx"p arams if needed!
-	      super(_ctrls.activity);
-	      context   = _ctrls.activity;
-	      pascalObj = _Self;
-	      controls  = _ctrls;
-	      lparams = new LayoutParams(lparamW, lparamH);
-	      //lparams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);		
-	   } //end constructor
-
-	   public void jFree() {
-	      if (parent != null) { parent.removeView(this); }
-	      //free local objects...      
-	      lparams = null;
-	      //parent = null;  //?!
-	   }
-
-	   public void SetViewParent(ViewGroup _viewgroup) {
-	      if (parent != null) { parent.removeView(this); }
-	      parent = _viewgroup;
-	      parent.addView(this,lparams);
-	      mRemovedFromParent = false; //now there is a parent!    	
-	   }
-	   	  				   
-	   public void RemoveFromViewParent() {
-	      if (!mRemovedFromParent) {
-	    	 this.setVisibility(android.view.View.INVISIBLE);
-	    	 ViewGroup parent1 = (ViewGroup)this.getParent();	    	 	    	
-	    	 if (parent1 != null) {
-	            parent1.removeView(this);
-	            parent = null;
-	            mRemovedFromParent = true; //no more parent!
-	    	 }          
-	      }
-	   }
-	   
-	   public View GetView() {
-	      return this;
-	   }
-	   
-	   public void SetLParamWidth(int _w) {
-	      lparamW = _w;
-	   }
-
-	   public void SetLParamHeight(int _h) {
-	      lparamH = _h;
-	   }
-
-	   public void SetLeftTopRightBottomWidthHeight(int _left, int _top, int _right, int _bottom, int _w, int _h) {
-	      marginLeft = _left;
-	      marginTop = _top;
-	      marginRight = _right;
-	      marginBottom = _bottom;
-	      lparamH = _h;
-	      lparamW = _w;
-	   }
-
-	   public void AddLParamsAnchorRule(int _rule) {
-	      lparamsAnchorRule[countAnchorRule] = _rule;
-	      countAnchorRule = countAnchorRule + 1;
-	   }
-
-	   public void AddLParamsParentRule(int _rule) {
-	      lparamsParentRule[countParentRule] = _rule;
-	      countParentRule = countParentRule + 1;
-	   }
-
-	   public void SetLayoutAll(int _idAnchor) {
-	  	 lparams.width  = lparamW;
-		 lparams.height = lparamH;
-		 lparams.setMargins(marginLeft,marginTop,marginRight,marginBottom);
-		 if (_idAnchor > 0) {
-		    for (int i=0; i < countAnchorRule; i++) {
-			  lparams.addRule(lparamsAnchorRule[i], _idAnchor);
-		    }
-		 }
-	     for (int j=0; j < countParentRule; j++) {
-	         lparams.addRule(lparamsParentRule[j]);
-	     }
-	     this.setLayoutParams(lparams);
-	   }
-
-	   public void ClearLayoutAll() {
-		  for (int i=0; i < countAnchorRule; i++) {
-	  	    lparams.removeRule(lparamsAnchorRule[i]);
-	      }
-
-		  for (int j=0; j < countParentRule; j++) {
-	   	    lparams.removeRule(lparamsParentRule[j]);
-		  }
-		  countAnchorRule = 0;
-		  countParentRule = 0;
-	   }
-
-	   public void SetId(int _id) { //wrapper method pattern ...
-	      this.setId(_id);
-	   }
-	   			
-       public void Show() {	//0: visible; 4: invisible; 8: gone		  
-		  this.setVisibility(android.view.View.VISIBLE);
-	   }
-				
-	   public void Hide() { 
-		 if (this.getVisibility()==0) { //visible   
-			this.setVisibility(android.view.View.INVISIBLE); //4
-		 }    
-	   }	
-	                 
-} //end class
-
-
 //**new jclass entrypoint**//please, do not remove/change this line!
 
 //Javas/Pascal Interface Class 
@@ -12177,7 +12045,7 @@ public native void pOnShellCommandExecuted(long pasobj, String cmdResult);
 static {
     //Log.i("JNI_Java", "1.load libcontrols.so");
 
-    //System.loadLibrary("freetype"); // need by TFPNoGUIGraphicsBridge [ref. www.github.com/jmpessoa/tfpnoguigraphicsbridge]
+    System.loadLibrary("freetype"); // need by TFPNoGUIGraphicsBridge [ref. www.github.com/jmpessoa/tfpnoguigraphicsbridge]
 
     System.loadLibrary("controls");
 
@@ -13212,9 +13080,5 @@ public float[] benchMark1 () {
    public java.lang.Object jDigitalClock_jCreate(long _Self) {
 	      return (java.lang.Object)(new jDigitalClock(this,_Self));
    }
-   
-   public java.lang.Object jCustomViewLayout_jCreate(long _Self) {
-	      return (java.lang.Object)(new jCustomViewLayout(this,_Self));
-   }
-   
+      
 }
