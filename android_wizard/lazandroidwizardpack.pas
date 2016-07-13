f0af7e51f15c6bca773a744331074646b1240fe5
Jose Marques Pessoa <jmpessoa_hotmail.com>
2016-07-13 20:55:35 -0300

    LazAndroidWizardPack requirements
    Added CodeTools requirements to LazAndroidWizardPack [thanks to A.S.]

---
+++ android_wizard/lazandroidwizardpack.pas
@@ -0,0 +1,23 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit LazAndroidWizardPack;
+
+interface
+
+uses
+  AndroidWizard_intf, uformworkspace, uFormAndroidProject, uRegisterForm, 
+  FormPathMissing, uFormOSystem, uJavaParser, LamwDesigner, uFormSizeSelect, 
+  ImgCache, LamwSettings, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('AndroidWizard_intf', @AndroidWizard_intf.Register);
+end;
+
+initialization
+  RegisterPackage('LazAndroidWizardPack', @Register);
+end.
