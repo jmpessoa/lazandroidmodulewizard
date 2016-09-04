4632a83b22afb0186af811d99bc1ef12b349b49c
Jose Marques Pessoa <jmpessoa_hotmail.com>
2016-09-04 09:57:13 -0300

    Version 0.7.1 - 03 September - 2016 - **
    ** Try Fix register_extras.pas

--- android_bridges/register_extras.pas
+++ android_bridges/register_extras.pas
@@ -9,7 +9,6 @@ interface
 uses 
   Classes,
   activitylauncher,
-  activity,
   mediaplayer,
   mediarecorder,
   downloadmanager,
@@ -47,7 +46,6 @@ implementation
 Procedure Register;
 begin
   {$I jactivitylauncher_icon.lrs}
-  {$I jactivity_icon.lrs}
   {$I jdatepickerdialog_icon.lrs}
   {$I jtimepickerdialog_icon.lrs}
   {$I jDialogYN_icon.lrs}
@@ -86,7 +84,6 @@ begin
   RegisterComponents('Android Bridges Extra',
    [
       jActivityLauncher,
-      jActivity,
       jDatePickerDialog,
       jTimePickerDialog,
       jDialogYN,
