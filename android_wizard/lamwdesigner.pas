8074bf5acfabad21027ef84cdba7315e46cf4376
Anton <odisey1245@mail.ru>
2017-09-27 22:28:07 +0300

    * lamwDesigner: fix layout logic

--- android_wizard/lamwdesigner.pas
+++ android_wizard/lamwdesigner.pas
@@ -1744,31 +1744,6 @@ begin
   begin
     lpWidth := LayoutParamWidth;
     lpHeight := LayoutParamHeight;
-    if Anchor <> nil then
-    begin
-      if raBelow in PosRelativeToAnchor then
-        FLeftTop.y := Anchor.Top + Anchor.Height + Anchor.MarginBottom + MarginTop;
-      if raAbove in PosRelativeToAnchor then
-        FRightBottom.y := Anchor.Top - MarginBottom - Anchor.MarginTop;
-      if raToRightOf in PosRelativeToAnchor then
-        FLeftTop.x := Anchor.Left + Anchor.Width + Anchor.MarginRight + MarginLeft;
-      if raAlignBaseline in PosRelativeToAnchor then
-        FLeftTop.y := Anchor.Top + (Anchor.Height - Height) div 2; // hack
-      if raAlignLeft in PosRelativeToAnchor then
-        FLeftTop.x := Anchor.Left + MarginLeft;
-      if raAlignRight in PosRelativeToAnchor then
-        FRightBottom.x := Anchor.Left + Anchor.Width - MarginRight;
-      if raToEndOf in PosRelativeToAnchor then
-        FLeftTop.x := Anchor.Left + Anchor.Width + Anchor.MarginRight + MarginLeft;
-      if raAlignTop in PosRelativeToAnchor then
-        FLeftTop.y := Anchor.Top + MarginTop;
-      if raAlignBottom in PosRelativeToAnchor then
-        FRightBottom.y := Anchor.Top + Anchor.Height - MarginBottom;
-      { TODO: other combinations raToStartOf, raToLeftOf, raAlignStart, raAlignEnd }
-      if ([raBelow, raAlignBottom] * PosRelativeToAnchor <> [])
-      and Assigned(Parent) and (rpBottom in PosRelativeToParent) then
-        FRightBottom.y := Parent.Height - MarginBottom;
-    end;
     if Assigned(Parent) then
     begin
       case lpWidth of
@@ -1809,6 +1784,34 @@ begin
           if FMinHeight < FnewH then FMinHeight := FnewH;
         end;
       end;
+    end;
+    if Anchor <> nil then
+    begin
+      if raBelow in PosRelativeToAnchor then
+        FLeftTop.y := Anchor.Top + Anchor.Height + Anchor.MarginBottom + MarginTop;
+      if raAbove in PosRelativeToAnchor then
+        FRightBottom.y := Anchor.Top - MarginBottom - Anchor.MarginTop;
+      if raToRightOf in PosRelativeToAnchor then
+        FLeftTop.x := Anchor.Left + Anchor.Width + Anchor.MarginRight + MarginLeft;
+      if raAlignBaseline in PosRelativeToAnchor then
+        FLeftTop.y := Anchor.Top + (Anchor.Height - Height) div 2; // hack
+      if raAlignLeft in PosRelativeToAnchor then
+        FLeftTop.x := Anchor.Left + MarginLeft;
+      if raAlignRight in PosRelativeToAnchor then
+        FRightBottom.x := Anchor.Left + Anchor.Width - MarginRight;
+      if raToEndOf in PosRelativeToAnchor then
+        FLeftTop.x := Anchor.Left + Anchor.Width + Anchor.MarginRight + MarginLeft;
+      if raAlignTop in PosRelativeToAnchor then
+        FLeftTop.y := Anchor.Top + MarginTop;
+      if raAlignBottom in PosRelativeToAnchor then
+        FRightBottom.y := Anchor.Top + Anchor.Height - MarginBottom;
+      { TODO: other combinations raToStartOf, raToLeftOf, raAlignStart, raAlignEnd }
+      if ([raBelow, raAlignBottom] * PosRelativeToAnchor <> [])
+      and Assigned(Parent) and (rpBottom in PosRelativeToParent) then
+        FRightBottom.y := Parent.Height - MarginBottom;
+    end;
+    if Assigned(Parent) then
+    begin
       if rpRight in PosRelativeToParent then
         FRightBottom.x := Parent.Width - MarginRight;
       if rpLeft in PosRelativeToParent then
