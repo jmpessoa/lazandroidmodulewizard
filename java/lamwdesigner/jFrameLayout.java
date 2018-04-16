e748826129d953225eaf003465350756a80d2898
Jose Marques Pessoa <jmpessoa_hotmail.com>
2018-04-16 01:46:29 -0300

    Add missing jLinear/jFrame/jCaptionPanel java stuff...

---
+++ java/lamwdesigner/jFrameLayout.java
@@ -0,0 +1,114 @@
+package org.lamw.appframelayoutdemo1;
+
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+/*Draft java code by "Lazarus Android Module Wizard" [12/13/2017 17:18:12]*/
+/*https://github.com/jmpessoa/lazandroidmodulewizard*/
+/*jVisualControl LAMW template*/
+ 
+public class jFrameLayout extends FrameLayout /*dummy*/ { //please, fix what GUI object will be extended!
+
+   private long pascalObj = 0;        // Pascal Object
+   private Controls controls  = null; //Java/Pascal [events] Interface ...
+   private jCommons LAMWCommon;
+   private Context context = null;
+
+   private OnClickListener onClickListener;   // click event
+   private Boolean enabled  = true;           // click-touch enabled!
+
+   //GUIDELINE: please, preferentially, init all yours params names with "_", ex: int _flag, String _hello ...
+   public jFrameLayout(Controls _ctrls, long _Self) { //Add more others news "_xxx" params if needed!
+
+      super(_ctrls.activity);
+      context   = _ctrls.activity;
+      pascalObj = _Self;
+      controls  = _ctrls;
+
+      LAMWCommon = new jCommons(this,context,pascalObj);
+
+      onClickListener = new OnClickListener(){
+      /*.*/public void onClick(View view){     // *.* is a mask to future parse...;
+              if (enabled) {
+                 controls.pOnClickGeneric(pascalObj, Const.Click_Default); //JNI event onClick!
+              }
+           };
+      };
+      setOnClickListener(onClickListener);
+   } //end constructor
+
+   public void jFree() {
+      //free local objects...
+  	 setOnClickListener(null);
+	 LAMWCommon.free();
+   }
+ 
+   public void SetViewParent(ViewGroup _viewgroup) {
+	 LAMWCommon.setParent(_viewgroup);
+   }
+
+   public ViewGroup GetParent() {
+      return LAMWCommon.getParent();
+   }
+
+   public void RemoveFromViewParent() {
+  	 LAMWCommon.removeFromViewParent();
+   }
+
+   public View GetView() {
+      return this;
+   }
+
+   public void SetLParamWidth(int _w) {
+  	 LAMWCommon.setLParamWidth(_w);
+   }
+
+   public void SetLParamHeight(int _h) {
+  	 LAMWCommon.setLParamHeight(_h);
+   }
+
+   public int GetLParamWidth() {
+      return LAMWCommon.getLParamWidth();
+   }
+
+   public int GetLParamHeight() {
+	 return  LAMWCommon.getLParamHeight();
+   }
+
+   public void SetLGravity(int _g) {
+  	 LAMWCommon.setLGravity(_g);
+   }
+
+   public void SetLWeight(float _w) {
+  	 LAMWCommon.setLWeight(_w);
+   }
+
+   public void SetLeftTopRightBottomWidthHeight(int _left, int _top, int _right, int _bottom, int _w, int _h) {
+      LAMWCommon.setLeftTopRightBottomWidthHeight(_left,_top,_right,_bottom,_w,_h);
+   }
+
+   public void AddLParamsAnchorRule(int _rule) {
+	 LAMWCommon.addLParamsAnchorRule(_rule);
+   }
+
+   public void AddLParamsParentRule(int _rule) {
+	 LAMWCommon.addLParamsParentRule(_rule);
+   }
+
+   public void SetLayoutAll(int _idAnchor) {
+  	 LAMWCommon.setLayoutAll(_idAnchor);
+   }
+
+   public void ClearLayoutAll() {
+	 LAMWCommon.clearLayoutAll();
+   }
+
+   //GUIDELINE: please, preferentially, init all yours params names with "_", ex: int _flag, String _hello ...
+   public void SetId(int _id) { //wrapper method pattern ...
+      this.setId(_id);
+   }
+
+}
