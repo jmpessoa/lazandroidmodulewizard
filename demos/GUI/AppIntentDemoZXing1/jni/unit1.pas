9e52d626d5d160eec9264a97cc50c84a5e0f0ee4
Jose Marques Pessoa <jmpessoa_hotmail.com>
2019-03-27 17:08:52 -0300

    Updated demo "AppIntentDemoZXing1"
    [added runtime permission]

--- demos/GUI/AppIntentDemoZXing1/jni/unit1.pas
+++ demos/GUI/AppIntentDemoZXing1/jni/unit1.pas
@@ -14,12 +14,18 @@ type
   { TAndroidModule1 }
 
   TAndroidModule1 = class(jForm)
+    jCheckBox1: jCheckBox;
+    jCheckBox2: jCheckBox;
     jIntentManager1: jIntentManager;
     jListView1: jListView;
     jTextView1: jTextView;
     jTextView2: jTextView;
     procedure AndroidModule1ActivityResult(Sender: TObject;
       requestCode: integer; resultCode: TAndroidResult; intentData: jObject);
+    procedure AndroidModule1JNIPrompt(Sender: TObject);
+    procedure AndroidModule1RequestPermissionResult(Sender: TObject;
+      requestCode: integer; manifestPermission: string;
+      grantResult: TManifestPermissionResult);
     procedure jListView1ClickItem(Sender: TObject; itemIndex: integer;
       itemCaption: string);
   private
@@ -42,13 +48,14 @@ implementation
 procedure TAndroidModule1.jListView1ClickItem(Sender: TObject;
   itemIndex: integer; itemCaption: string);
 begin
-  //if jListView1.IsItemChecked(Item) then
-  //begin
-     jIntentManager1.SetAction('com.google.zxing.client.android.SCAN');
-     if  jIntentManager1.IsCallable(jIntentManager1.GetIntent()) then
+  if IsRuntimePermissionGranted('android.permission.CAMERA') then
+  begin
+     if jIntentManager1.IsCallable('com.google.zxing.client.android.SCAN') then
      begin
+
          //ShowMessage('ZXing App is Installed!! ');
 
+         jIntentManager1.SetAction('com.google.zxing.client.android.SCAN');
          case itemIndex of
             0: jIntentManager1.PutExtraString('SCAN_MODE', 'PRODUCT_MODE');   //Prod
             1: jIntentManager1.PutExtraString('SCAN_MODE', 'QR_CODE_MODE');   //QR
@@ -59,16 +66,21 @@ begin
          jIntentManager1.StartActivityForResult(0);      //user-defined requestCode= 0
 
      end
-     else  //download ZXing
+     else  //download ZXing App
      begin
-        ShowMessage('Try downloading   ZXing App ...');
-        if not Self.IsWifiEnabled() then Self.SetWifiEnabled(True);
-        jIntentManager1.SetAction(jIntentManager1.GetActionViewAsString());
-        //or jIntentManager1.SetAction('android.intent.action.VIEW');
-        jIntentManager1.SetDataUri(jIntentManager1.ParseUri('market://search?q=pname:'+'com.google.zxing.client.android'));
-        jIntentManager1.StartActivity();
+        if Self.IsWifiEnabled() then
+        begin
+          ShowMessage('Wait... try downloading ZXing App fro Google Play Store ...');
+          jIntentManager1.SetAction(jIntentManager1.GetActionViewAsString());
+          //or jIntentManager1.SetAction('android.intent.action.VIEW');
+
+          jIntentManager1.SetDataUri(jIntentManager1.ParseUri('market://search?q=pname:'+'com.google.zxing.client.android'));
+          jIntentManager1.StartActivity();
+        end
+        else ShowMessage('Please, enable wifi and try again....');
      end;
-  //end;
+  end
+  else ShowMessage('Sorry... Some Runtime Permission NOT Granted ...');
 end;
 
 procedure TAndroidModule1.AndroidModule1ActivityResult(Sender: TObject;
@@ -81,6 +93,46 @@ begin
         ShowMessage('FORMAT: '+jIntentManager1.GetExtraString(intentData, 'SCAN_RESULT_FORMAT'));
         ShowMessage('CONTENT: '+jIntentManager1.GetExtraString(intentData, 'SCAN_RESULT'));
     end else ShowMessage('Fail/cancel to scan....');
+  end;
+end;
+
+//https://developer.android.com/guide/topics/security/permissions#normal-dangerous
+procedure TAndroidModule1.AndroidModule1JNIPrompt(Sender: TObject);
+begin
+   Self.SetWifiEnabled(True);  //try internet connection... need to get ZXing App from Google Play Stores)
+
+   if Self.IsWifiEnabled() then
+   begin
+      jCheckBox2.Checked:= True;
+   end;
+
+   if IsRuntimePermissionNeed() then   // that is, if target API >= 23
+   begin
+     ShowMessage('Requesting Runtime Permission....');
+     Self.RequestRuntimePermission('android.permission.CAMERA', 1111);   //handled by OnRequestPermissionResult
+   end
+   else
+   begin
+     jCheckBox1.Checked:= True;
+   end;
+end;
+
+procedure TAndroidModule1.AndroidModule1RequestPermissionResult(
+  Sender: TObject; requestCode: integer; manifestPermission: string;
+  grantResult: TManifestPermissionResult);
+begin
+   case requestCode of
+    1111:begin
+           if grantResult = PERMISSION_GRANTED  then
+           begin
+              if manifestPermission = 'android.permission.CAMERA' then jCheckBox1.Checked:= True;
+           end
+          else//PERMISSION_DENIED
+          begin
+              jCheckBox1.Checked:= False;
+              ShowMessage('Sorry... "android.permission.CAMERA" permission not granted... ' );
+          end;
+       end;
   end;
 end;
 
