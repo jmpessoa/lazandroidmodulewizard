81306b91f3497a337badd59285b3ba073cf91138
tr3esoftware <44481313+tr3esoftware@users.noreply.github.com>
2019-08-14 11:09:15 +0200

    Revised and improved "jDialogYN" [by TR3E]
    Revised and improved "jDialogYN" [by TR3E]
    Now you can define the font size, with the new function "SetFontSize"

--- android_wizard/smartdesigner/java/jDialogYN.java
+++ android_wizard/smartdesigner/java/jDialogYN.java
@@ -2,65 +2,55 @@ package org.lamw.appwidgetproviderdemo1;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.Gravity;
 
 class YNConst {
   public static final int Click_Yes = -1;
   public static final int Click_No  = -2;
 }
 
-public class jDialogYN {
-//Java-Pascal Interface
-private long             PasObj   = 0;      // Pascal Obj
-private Controls        controls = null;   // Control Class for Event
-//
-private String          dlgTitle;
-private String          dlgMsg;
-private String          dlgY;
-private String          dlgN;
-//
-private DialogInterface.OnClickListener onClickListener = null;
-private AlertDialog dialog = null;
+//Revised 08/14/2019 [by TR3E]
 
-//Constructor
-public  jDialogYN(android.content.Context context,
+public class jDialogYN {
+ //Java-Pascal Interface
+ private long             PasObj  = 0;      // Pascal Obj
+ private Controls        controls = null;   // Control Class for Event
+ //
+ private String          dlgTitle;
+ private String          dlgMsg;
+ private String          dlgY;
+ private String          dlgN;
+ 
+ private int 			 mFontSize; 
+ //
+ private DialogInterface.OnClickListener onClickListener = null;
+ private AlertDialog dialog = null;
+
+ //Constructor
+ public  jDialogYN(android.content.Context context,
               Controls ctrls, long pasobj,
               String title, String msg, String y, String n ) {
-//Connect Pascal I/F
-PasObj   = pasobj;
-controls = ctrls;
-//
-dlgTitle = title;
-dlgMsg   = msg;
-dlgY     = y;
-dlgN     = n;
+  //Connect Pascal I/F
+  PasObj   = pasobj;
+  controls = ctrls;
+  
+  //
+  dlgTitle = title;
+  dlgMsg   = msg;
+  dlgY     = y;
+  dlgN     = n;
+  
+  mFontSize = 0;
 
-//Init Event
-onClickListener = new DialogInterface.OnClickListener() {
-public  void onClick(DialogInterface dialog, int id) {
-  if  ( id == YNConst.Click_Yes) { controls.pOnClick(PasObj,YNConst.Click_Yes);}
+  //Init Event
+  onClickListener = new DialogInterface.OnClickListener() {
+   public  void onClick(DialogInterface dialog, int id) {
+    if  ( id == YNConst.Click_Yes) { controls.pOnClick(PasObj,YNConst.Click_Yes);}
                           else { controls.pOnClick(PasObj,YNConst.Click_No );}
-};
-};
-//Init Class
-if (dlgY.equals("")) dlgY ="Yes";
-if (dlgN.equals("")) dlgN ="No";
-
-AlertDialog.Builder builder = new AlertDialog.Builder(controls.activity);
-builder.setMessage       (dlgMsg  )
-     .setCancelable    (false)
-     .setPositiveButton(dlgY,onClickListener)
-     .setNegativeButton(dlgN,onClickListener);
-dialog = builder.create();
-//
-dialog.setTitle(dlgTitle);
-//dialog.setIcon(R.drawable.icon);  //my comment here!!
-
-}
-
-public  void show() {
- //Log.i("java","DlgYN_Show");
- dialog.show();
+   };
+  };
 }
 
 public  void show(String titleText, String msgText, String yesText, String noText) {
@@ -74,34 +64,39 @@ public  void show(String titleText, String msgText, String yesText, String noTex
 	if (dlgN.equals("")) dlgN ="No";
 	
 	AlertDialog.Builder builder = new AlertDialog.Builder(controls.activity);
-	builder.setMessage       (dlgMsg  )
-	       .setCancelable    (false)
-	       .setPositiveButton(dlgY,onClickListener)
-	       .setNegativeButton(dlgN,onClickListener);
-	dialog = builder.create();
-	//
-	dialog.setTitle(dlgTitle);
-
-	dialog.show();
-}
-
-public void show(String titleText, String msgText) {
-	   //Log.i("java","DlgYN_Show");
-	dlgTitle = titleText;
-	dlgMsg   = msgText;
-
-	if (dlgY.equals("")) dlgY ="Yes";
-	if (dlgN.equals("")) dlgN ="No";
 	
-	AlertDialog.Builder builder = new AlertDialog.Builder(controls.activity);
+	
+	TextView title = new TextView(controls.activity);
+    title.setText(dlgTitle);
+    title.setPadding(5, 5, 5, 5);
+    //title.setBackgroundResource(R.drawable.gradient);
+    //title.setGravity(Gravity.CENTER);
+    //title.setTextColor(0xFF0000FF);
+    if( mFontSize != 0)
+    	  title.setTextSize(mFontSize);
+    
+    builder.setCustomTitle(title);
+	
 	builder.setMessage       (dlgMsg  )
 	       .setCancelable    (false)
 	       .setPositiveButton(dlgY,onClickListener)
 	       .setNegativeButton(dlgN,onClickListener);
-	dialog = builder.create();
-	//
-	dialog.setTitle(dlgTitle);
+	dialog = builder.create();	
+
 	dialog.show();
+	
+	if( mFontSize != 0){
+		  dialog.getWindow().getAttributes();
+			 
+		  TextView tvMessage = (TextView) dialog.findViewById(android.R.id.message);
+		  tvMessage.setTextSize(mFontSize);
+		 
+		  Button btNegative = dialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+		  btNegative.setTextSize(mFontSize);
+		 
+		  Button btPositive = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+		  btPositive.setTextSize(mFontSize);
+		 }
 }
 
 
@@ -110,21 +105,47 @@ public void ShowOK(String titleText, String msgText, String _OkText) {
 	dlgTitle = titleText;
 	dlgMsg   = msgText;	
 	AlertDialog.Builder builder = new AlertDialog.Builder(controls.activity);
+	
+	TextView title = new TextView(controls.activity);
+    title.setText(dlgTitle);
+    title.setPadding(5, 5, 5, 5);
+    
+    if( mFontSize != 0)
+    	  title.setTextSize(mFontSize);
+    
+    builder.setCustomTitle(title);
+	
 	builder.setMessage       (dlgMsg  )
 	       .setCancelable    (false)	       
 	       .setPositiveButton(_OkText, onClickListener);
 	       	      
 	dialog = builder.create();
-	dialog.setTitle(dlgTitle);
+	
 	dialog.show();
+	
+	if( mFontSize != 0){
+		  dialog.getWindow().getAttributes();
+			 
+		  TextView tvMessage = (TextView) dialog.findViewById(android.R.id.message);
+		  tvMessage.setTextSize(mFontSize);
+		 
+		  Button btPositive = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+		  btPositive.setTextSize(mFontSize);		 		 
+		 }
 }
 
+// by tr3e
+public void SetFontSize( int _fontSize ){
+	mFontSize = _fontSize;
+}
 
 public  void Free() {
-onClickListener = null;
-dialog.setTitle("");
-dialog.setIcon(null);
-dialog = null;
+ onClickListener = null;
+ 
+ dialog.setTitle("");
+ dialog.setIcon(null);
+ 
+ dialog = null;
 }
 
 }
